4bfccebf6ebc16a94010f943966b14d646b8d8f7
yurian51 <yurianarshad@gmail.com>
2026-09-13 08:35:56 +0300

    feat: register network capability controller

--- apps/api/src/routers/routers.module.ts
+++ apps/api/src/routers/routers.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuditModule } from '../audit/audit.module';
+import { NetworkCapabilitiesController } from './network-capabilities.controller';
 import { RoutersController } from './routers.controller';
 import { RoutersService } from './routers.service';
 
 @Module({
   imports: [AuditModule],
-  controllers: [RoutersController],
+  controllers: [RoutersController, NetworkCapabilitiesController],
   providers: [RoutersService],
   exports: [RoutersService],
 })
