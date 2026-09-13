c532521758498f7f16bc1ed77d82218617c5a8d8
yurian51 <yurianarshad@gmail.com>
2026-09-13 08:46:18 +0300

    feat(network): register credential encryption service

--- apps/api/src/routers/routers.module.ts
+++ apps/api/src/routers/routers.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuditModule } from '../audit/audit.module';
+import { SecureNetworkCredentials } from '../common/secure-network-credentials';
 import { NetworkCapabilitiesController } from './network-capabilities.controller';
 import { RoutersController } from './routers.controller';
 import { RoutersService } from './routers.service';
 
 @Module({
-  imports: [AuditModule],
+  imports: [AuditModule, ConfigModule],
   controllers: [RoutersController, NetworkCapabilitiesController],
-  providers: [RoutersService],
-  exports: [RoutersService],
+  providers: [SecureNetworkCredentials, RoutersService],
+  exports: [RoutersService, SecureNetworkCredentials],
 })
 export class RoutersModule {}
