e7f52d7166dc6ccd744ba7a6a93922fdce3e102c
yurian51 <yurianarshad@gmail.com>
2026-09-13 08:19:02 +0300

    feat: register vendor-neutral wifi device adapter registry

--- apps/api/src/modules/traffic/traffic.module.ts
+++ apps/api/src/modules/traffic/traffic.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { FairnessService } from './fairness.service';
 import { TrafficEnforcementService } from './enforcement.service';
 import { MikroTikTrafficEnforcementAdapter } from './mikrotik.adapter';
+import { NetworkDeviceAdapterRegistry } from './network-device.adapter';
 import { TrafficSamplesController } from './traffic-samples.controller';
 import { TrafficSamplesService } from './traffic-samples.service';
 import { TrafficOrchestratorService } from './traffic-orchestrator.service';
@@ -14,6 +15,7 @@ import { TrafficCollectorService } from './traffic-collector.service';
     FairnessService,
     TrafficSamplesService,
     MikroTikTrafficEnforcementAdapter,
+    NetworkDeviceAdapterRegistry,
     {
       provide: TrafficEnforcementService,
       useFactory: (fairnessService: FairnessService, adapter: MikroTikTrafficEnforcementAdapter) =>
@@ -23,6 +25,6 @@ import { TrafficCollectorService } from './traffic-collector.service';
     TrafficOrchestratorService,
     TrafficCollectorService,
   ],
-  exports: [FairnessService, TrafficEnforcementService, TrafficSamplesService, TrafficOrchestratorService],
+  exports: [FairnessService, TrafficEnforcementService, TrafficSamplesService, TrafficOrchestratorService, NetworkDeviceAdapterRegistry],
 })
 export class TrafficModule {}
