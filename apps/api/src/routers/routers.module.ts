e817216b1eb6e7f3fc95faad2c50f30390698cea
yurian51 <yurianarshad@gmail.com>
2026-09-13 07:12:05 +0300

    feat: wire router auditing

--- apps/api/src/routers/routers.module.ts
+++ apps/api/src/routers/routers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuditModule } from '../audit/audit.module';
 import { RoutersController } from './routers.controller';
 import { RoutersService } from './routers.service';
 
 @Module({
+  imports: [AuditModule],
   controllers: [RoutersController],
   providers: [RoutersService],
   exports: [RoutersService],
