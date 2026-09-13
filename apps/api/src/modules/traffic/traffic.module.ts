83173bab830c050c281fd77955c88d813e817c97
yurian51 <yurianarshad@gmail.com>
2026-09-13 07:28:48 +0300

    feat(traffic): register enforcement service in module

--- apps/api/src/modules/traffic/traffic.module.ts
+++ apps/api/src/modules/traffic/traffic.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FairnessService } from './fairness.service';
+import { TrafficEnforcementService } from './enforcement.service';
+import { NoopTrafficEnforcementAdapter } from './enforcement.adapter';
 
 @Module({
-  providers: [FairnessService],
-  exports: [FairnessService],
+  providers: [
+    FairnessService,
+    NoopTrafficEnforcementAdapter,
+    {
+      provide: TrafficEnforcementService,
+      useFactory: (fairnessService: FairnessService, adapter: NoopTrafficEnforcementAdapter) =>
+        new TrafficEnforcementService(fairnessService, adapter),
+      inject: [FairnessService, NoopTrafficEnforcementAdapter],
+    },
+  ],
+  exports: [FairnessService, TrafficEnforcementService],
 })
 export class TrafficModule {}
