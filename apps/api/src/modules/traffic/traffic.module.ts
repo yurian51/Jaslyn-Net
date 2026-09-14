539b38491b6eec156c969078ad99f4174b57e238
yurian51 <yurianarshad@gmail.com>
2026-09-14 06:41:39 +0300

    feat: register Meraki traffic enforcement adapter

--- apps/api/src/modules/traffic/traffic.module.ts
+++ apps/api/src/modules/traffic/traffic.module.ts
@@ -3,6 +3,7 @@ import { SecureNetworkCredentials } from '../../common/secure-network-credential
 import { FairnessService } from './fairness.service';
 import { TrafficEnforcementService } from './enforcement.service';
 import { MikroTikTrafficEnforcementAdapter } from './mikrotik.adapter';
+import { MerakiTrafficEnforcementAdapter } from './meraki.adapter';
 import { NetworkDeviceAdapterRegistry } from './network-device.adapter';
 import { TrafficSamplesController } from './traffic-samples.controller';
 import { TrafficSamplesService } from './traffic-samples.service';
@@ -17,11 +18,19 @@ import { TrafficCollectorService } from './traffic-collector.service';
     FairnessService,
     TrafficSamplesService,
     MikroTikTrafficEnforcementAdapter,
+    MerakiTrafficEnforcementAdapter,
     NetworkDeviceAdapterRegistry,
     {
       provide: TrafficEnforcementService,
-      useFactory: (fairnessService: FairnessService, adapter: MikroTikTrafficEnforcementAdapter) => new TrafficEnforcementService(fairnessService, adapter),
-      inject: [FairnessService, MikroTikTrafficEnforcementAdapter],
+      useFactory: (
+        fairnessService: FairnessService,
+        mikrotik: MikroTikTrafficEnforcementAdapter,
+        meraki: MerakiTrafficEnforcementAdapter,
+      ) => new TrafficEnforcementService(fairnessService, {
+        MIKROTIK_REST: mikrotik,
+        MERAKI_DASHBOARD_API: meraki,
+      }),
+      inject: [FairnessService, MikroTikTrafficEnforcementAdapter, MerakiTrafficEnforcementAdapter],
     },
     TrafficOrchestratorService,
     TrafficCollectorService,
