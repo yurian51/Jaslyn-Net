19e9bfa2af4c3fc608a5eb2e06a8757520a0e281
yurian51 <yurianarshad@gmail.com>
2026-09-15 10:16:47 +0300

    feat(purchases): enforce payment provider settlement boundary

--- apps/api/src/purchases/purchases.module.ts
+++ apps/api/src/purchases/purchases.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PaymentsModule } from '../payments/payments.module';
 import { PurchasesController } from './purchases.controller';
 import { PurchasesService } from './purchases.service';
 
 @Module({
+  imports: [PaymentsModule],
   controllers: [PurchasesController],
   providers: [PurchasesService],
   exports: [PurchasesService],
