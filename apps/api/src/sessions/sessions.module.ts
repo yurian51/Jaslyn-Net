b67a0578095afb5c71352d425d54e5bfc365df5b
yurian51 <yurianarshad@gmail.com>
2026-09-16 19:44:51 +0300

    feat(sessions): wire automatic reconciliation worker

--- apps/api/src/sessions/sessions.module.ts
+++ apps/api/src/sessions/sessions.module.ts
@@ -4,11 +4,12 @@ import { RoutersModule } from '../routers/routers.module';
 import { TrafficModule } from '../modules/traffic/traffic.module';
 import { SessionsController } from './sessions.controller';
 import { SessionsService } from './sessions.service';
+import { ReconciliationWorker } from './reconciliation.worker';
 
 @Module({
   imports: [AuditModule, RoutersModule, TrafficModule],
   controllers: [SessionsController],
-  providers: [SessionsService],
+  providers: [SessionsService, ReconciliationWorker],
   exports: [SessionsService],
 })
 export class SessionsModule {}
