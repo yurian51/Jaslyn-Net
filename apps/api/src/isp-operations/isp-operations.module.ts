19d985a02a9c7095fd9da8dbcd1c1366ef185b96
yurian51 <yurianarshad@gmail.com>
2026-09-17 21:34:19 +0300

    feat: register ISP expiry reconciler

--- apps/api/src/isp-operations/isp-operations.module.ts
+++ apps/api/src/isp-operations/isp-operations.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { IspOperationsController } from './isp-operations.controller';
 import { CustomerServiceStateService } from './customer-service-state.service';
 import { FiberLinesService } from './fiber-lines.service';
+import { IspExpiryService } from './isp-expiry.service';
 import { IspOperationsService } from './isp-operations.service';
 
 @Module({
   controllers: [IspOperationsController],
-  providers: [IspOperationsService, FiberLinesService, CustomerServiceStateService],
-  exports: [IspOperationsService, FiberLinesService, CustomerServiceStateService],
+  providers: [IspOperationsService, FiberLinesService, CustomerServiceStateService, IspExpiryService],
+  exports: [IspOperationsService, FiberLinesService, CustomerServiceStateService, IspExpiryService],
 })
 export class IspOperationsModule {}
