537ee82c60ab604069ce8a38513cb315fa0f25b3
yurian51 <yurianarshad@gmail.com>
2026-09-18 01:15:19 +0300

    chore: wire ISP operations to audit module

--- apps/api/src/isp-operations/isp-operations.module.ts
+++ apps/api/src/isp-operations/isp-operations.module.ts
@@ -5,9 +5,10 @@ import { FiberLinesService } from './fiber-lines.service';
 import { IspExpiryService } from './isp-expiry.service';
 import { IspOperationsService } from './isp-operations.service';
 import { SessionsModule } from '../sessions/sessions.module';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
-  imports: [SessionsModule],
+  imports: [SessionsModule, AuditModule],
   controllers: [IspOperationsController],
   providers: [IspOperationsService, FiberLinesService, CustomerServiceStateService, IspExpiryService],
   exports: [IspOperationsService, FiberLinesService, CustomerServiceStateService, IspExpiryService],
