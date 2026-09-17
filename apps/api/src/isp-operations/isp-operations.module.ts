1f4f27e4cf729d7f702b24aa3a800d51eb861104
yurian51 <yurianarshad@gmail.com>
2026-09-17 21:04:46 +0300

    feat(api): register ISP operations controller

--- apps/api/src/isp-operations/isp-operations.module.ts
+++ apps/api/src/isp-operations/isp-operations.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { IspOperationsController } from './isp-operations.controller';
 import { IspOperationsService } from './isp-operations.service';
 
 @Module({
+  controllers: [IspOperationsController],
   providers: [IspOperationsService],
   exports: [IspOperationsService],
 })
