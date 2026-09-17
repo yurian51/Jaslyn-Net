b0623f8a162bea752f8b03fd5ee07484a10912cb
yurian51 <yurianarshad@gmail.com>
2026-09-17 12:28:24 +0300

    feat(operations): connect router state to incidents

--- apps/api/src/routers/routers.module.ts
+++ apps/api/src/routers/routers.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuditModule } from '../audit/audit.module';
+import { IncidentsModule } from '../incidents/incidents.module';
 import { SecureNetworkCredentials } from '../common/secure-network-credentials';
 import { NetworkCapabilitiesController } from './network-capabilities.controller';
 import { RoutersController } from './routers.controller';
@@ -9,7 +10,7 @@ import { NetworkEnforcementService } from './network-enforcement.service';
 import { RoutersService } from './routers.service';
 
 @Module({
-  imports: [AuditModule, ConfigModule],
+  imports: [AuditModule, ConfigModule, IncidentsModule],
   controllers: [RoutersController, NetworkCapabilitiesController],
   providers: [SecureNetworkCredentials, MikrotikRestAdapter, NetworkEnforcementService, RoutersService],
   exports: [RoutersService, SecureNetworkCredentials, NetworkEnforcementService],
