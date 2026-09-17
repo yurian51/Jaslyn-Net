5307a8503c9fe27f2b56c8cd0e0d967d1754dd52
yurian51 <yurianarshad@gmail.com>
2026-09-17 21:07:54 +0300

    fix(api): register customer service state service

--- apps/api/src/isp-operations/isp-operations.module.ts
+++ apps/api/src/isp-operations/isp-operations.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { IspOperationsController } from './isp-operations.controller';
+import { CustomerServiceStateService } from './customer-service-state.service';
 import { FiberLinesService } from './fiber-lines.service';
 import { IspOperationsService } from './isp-operations.service';
 
 @Module({
   controllers: [IspOperationsController],
-  providers: [IspOperationsService, FiberLinesService],
-  exports: [IspOperationsService, FiberLinesService],
+  providers: [IspOperationsService, FiberLinesService, CustomerServiceStateService],
+  exports: [IspOperationsService, FiberLinesService, CustomerServiceStateService],
 })
 export class IspOperationsModule {}
