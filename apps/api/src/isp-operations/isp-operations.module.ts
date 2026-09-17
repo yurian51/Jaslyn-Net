05a4db7942ee72aee576b2c37ccd6866aca00aeb
yurian51 <yurianarshad@gmail.com>
2026-09-17 21:03:20 +0300

    fix(api): register ISP operations service

--- apps/api/src/isp-operations/isp-operations.module.ts
+++ apps/api/src/isp-operations/isp-operations.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { IspOperationsService } from './isp-operations.service';
 
-@Module({})
+@Module({
+  providers: [IspOperationsService],
+  exports: [IspOperationsService],
+})
 export class IspOperationsModule {}
