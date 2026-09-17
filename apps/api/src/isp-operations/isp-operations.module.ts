ff06170feded4f3be95a4b051f92c73dca78f1f5
yurian51 <yurianarshad@gmail.com>
2026-09-17 21:37:57 +0300

    chore: provide sessions enforcement to ISP expiry worker

--- apps/api/src/isp-operations/isp-operations.module.ts
+++ apps/api/src/isp-operations/isp-operations.module.ts
@@ -4,8 +4,10 @@ import { CustomerServiceStateService } from './customer-service-state.service';
 import { FiberLinesService } from './fiber-lines.service';
 import { IspExpiryService } from './isp-expiry.service';
 import { IspOperationsService } from './isp-operations.service';
+import { SessionsModule } from '../sessions/sessions.module';
 
 @Module({
+  imports: [SessionsModule],
   controllers: [IspOperationsController],
   providers: [IspOperationsService, FiberLinesService, CustomerServiceStateService, IspExpiryService],
   exports: [IspOperationsService, FiberLinesService, CustomerServiceStateService, IspExpiryService],
