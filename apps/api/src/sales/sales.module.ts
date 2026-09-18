2771fa602d1e76e361c1c3aa00cc7859c2e512f1
yurian51 <yurianarshad@gmail.com>
2026-09-18 16:56:07 +0300

    feat(sales): add sales operations module

---
+++ apps/api/src/sales/sales.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { SalesController } from './sales.controller';
+import { SalesService } from './sales.service';
+
+@Module({ controllers: [SalesController], providers: [SalesService], exports: [SalesService] })
+export class SalesModule {}
