c946e960057549e6b11c813bd2008adf337136bc
yurian51 <yurianarshad@gmail.com>
2026-09-18 12:55:55 +0300

    refactor(billing): inject authoritative payments module

--- apps/api/src/billing/billing.module.ts
+++ apps/api/src/billing/billing.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PurchasesModule } from '../purchases/purchases.module';
+import { PaymentsModule } from '../payments/payments.module';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
 
 @Module({
-  imports: [PurchasesModule],
+  imports: [PurchasesModule, PaymentsModule],
   controllers: [BillingController],
   providers: [BillingService],
   exports: [BillingService],
