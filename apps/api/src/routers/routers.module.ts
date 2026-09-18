02c9fae81175e6b9f434649fba4497c593d58782
yurian51 <yurianarshad@gmail.com>
2026-09-18 10:08:16 +0300

    fix: provide incidents service to routers module

--- apps/api/src/routers/routers.module.ts
+++ apps/api/src/routers/routers.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuditModule } from '../audit/audit.module';
 import { SecureNetworkCredentials } from '../common/secure-network-credentials';
 import { TrafficModule } from '../modules/traffic/traffic.module';
+import { IncidentsModule } from '../incidents/incidents.module';
 import { NetworkCapabilitiesController } from './network-capabilities.controller';
 import { RoutersController } from './routers.controller';
 import { NetworkEnforcementService } from './network-enforcement.service';
 import { RoutersService } from './routers.service';
 
 @Module({
-  imports: [AuditModule, TrafficModule],
+  imports: [AuditModule, TrafficModule, IncidentsModule],
   controllers: [RoutersController, NetworkCapabilitiesController],
   providers: [SecureNetworkCredentials, NetworkEnforcementService, RoutersService],
   exports: [RoutersService, SecureNetworkCredentials, NetworkEnforcementService],
